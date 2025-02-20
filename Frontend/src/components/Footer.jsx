03513c3ba19ce915b1df468575964283fee4f94c
Sahin Ali <sahinali1242@gmail.com>
2025-02-20 14:30:24 +0000

    added footer component

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Bolded by Blood. Empowering Lives Through Donation.</p>
+    </footer>
+  );
+}
+
+export default Footer;
