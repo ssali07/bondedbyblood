345a566507eb886dda59ba13d6a56d1319691541
Sahin Ali <sahinali1242@gmail.com>
2025-02-20 14:44:07 +0000

    added react-router facility

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Footer from './components/Footer'
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
+import LocateDonor from "./pages/LocateDonor";
+import RegisterDonation from "./pages/RegisterDonation";
+import About from "./pages/About";
 
 function App() {
-
+  useEffect(()=>{
+    alert("Backend Integration is in progress...")
+  },[])
   return (
-    <>
-      <Footer/>
-    </>
-  )
+    <div className="app">
+      <Header />
+      
+      <main>
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/locate-donor" element={<LocateDonor />} />
+          <Route path="/" element={<RegisterDonation />} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
